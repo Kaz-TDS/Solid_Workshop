5515fb2df8de7528539bc206f978844a06e2342e
Kazimierz Luska <kaz@Kazimierzs-MacBook-Pro.local>
2022-07-19 16:09:14 +0100

    Removing duplicate copyright notice from Default EnemySpawner

--- SOLID_Objective_05/Assets/Scripts/Factories/EnemySpawnerDefault.cs
+++ SOLID_Objective_05/Assets/Scripts/Factories/EnemySpawnerDefault.cs
@@ -6,14 +6,6 @@
  * Created and maintained by:
  * Kazimierz Luska (kazimierz.luska@tripledotstudios.com
  */
-/*
- * (c) 2022 Tripledot Studios, all rights reserved.
- * Tripledot Studios owns the copyright to this file and all of it's content.
- * The file may not be reproduced in any form without permission.
- * 
- * Created and maintained by:
- * Kazimierz Luska (kazimierz.luska@tripledotstudios.com
- */
 
 using System;
 using Tripledot.Adventure.Enemies;
